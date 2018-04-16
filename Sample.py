4da69715d797be191a9a9c51ac5b1849284b9656
bewbie97 <bonnie.mahmud@gmail.com>
2018-04-16 14:04:16 -0400

    Progress from yesterday

--- Sample.py
+++ Sample.py
@@ -1,10 +1,18 @@
-print("Hello World")
+#print("Hello World")
 
-name = "Henry sucks but so does Maahi n Tom"
-tame = "butts"
-print(name)
+#name = "Henry sucks but so does Maahi n Tom"
+#tame = "butts"
+#print(name)
 
-print(name[0:11:2])
+#print(name[0:11:2])
 
 
-print("Hello world from bonnie")
+#print("Hello world from bonnie")
+
+#for number in range(10,-10,-1):
+   # print(number/10)
+
+counter = 0
+while counter < 10:
+    print(counter)
+
